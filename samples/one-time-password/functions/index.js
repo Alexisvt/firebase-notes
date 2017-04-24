a942dce6e17e3a765461c7ee994e3a0764af1a91
Alexis <alexisvt@gmail.com>
2017-04-24 16:28:54 -0600

    adding user data

--- samples/one-time-password/functions/index.js
+++ samples/one-time-password/functions/index.js
@@ -1,12 +1,12 @@
+// @flow
+const serviceAccount = require('./service_account.json');
+const admin = require('firebase-admin');
 var functions = require('firebase-functions');
+const createUser = require('./create_user');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://one-time-password-67371.firebaseio.com'
 });
 
-exports.goodBye = functions.https.onRequest((req, resp) => {
-  resp.send("Good Bye from Google Cloud Function");
-})
+exports.createUser = functions.https.onRequest(createUser);
